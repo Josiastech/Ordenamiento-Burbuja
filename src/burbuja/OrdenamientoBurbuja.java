afc18f57b8b13ba9c7f44e561a3167e6e76ba62b
Josias Emanuel <josiastech@gmail.com>
2012-09-09 13:08:11 -0600

    Second Commit
    Formato & Javadoc

--- src/burbuja/OrdenamientoBurbuja.java
+++ src/burbuja/OrdenamientoBurbuja.java
@@ -1,67 +1,78 @@
-// Ordenamiento de los valores de un arreglo en orden ascendente.
+/**
+ * Ordenamiento de los Valores de un Arreglo en Orden Ascendente
+ */
 package burbuja;
 
 import java.awt.*;
 import javax.swing.*;
 
-
+/**
+ * 
+ * @author josiastech
+ *
+ */
 public class OrdenamientoBurbuja extends JApplet {
-	// inicializar subprograma 
-	   public void init()
-	   {
-	      JTextArea areaSalida = new JTextArea();
-	      Container contenedor = getContentPane();
-	      contenedor.add( areaSalida );
+	// inicializar subprograma
+	public void init() {
+		JTextArea areaSalida = new JTextArea();
+		Container contenedor = getContentPane();
+		contenedor.add(areaSalida);
 
-	      int arreglo[] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
+		int arreglo[] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
 
-	      String salida = "Elementos de datos en su orden original\n";
+		String salida = "Elementos de datos en su orden original\n";
 
-	      // anexar los valores originales al String salida
-	      for ( int contador = 0; contador < arreglo.length; contador++ )
-	         salida += "   " + arreglo[ contador ];
-	 
-	      ordenamBurbuja( arreglo );  // ordenar arreglo
+		// anexar los valores originales al String salida
+		for (int contador = 0; contador < arreglo.length; contador++)
+			salida += "   " + arreglo[contador];
 
-	      salida += "\n\nElementos de datos en orden ascendente\n";
+		ordenamBurbuja(arreglo); // ordenar arreglo
 
-	      // anexar los valores ordenados del arreglo al String salida
-	      for ( int contador = 0; contador < arreglo.length; contador++ ) 
-	         salida += "   " + arreglo[ contador ];
+		salida += "\n\nElementos de datos en orden ascendente\n";
 
-	      areaSalida.setText( salida );
+		// anexar los valores ordenados del arreglo al String salida
+		for (int contador = 0; contador < arreglo.length; contador++)
+			salida += "   " + arreglo[contador];
 
-	   } // fin del método init
+		areaSalida.setText(salida);
 
-	   // ordenar elementos del arreglo con el método burbuja
-	   public void ordenamBurbuja( int arreglo2[] )
-	   {   
-	      // ciclo para controlar número de pasadas
-	      for ( int pasada = 1; pasada < arreglo2.length; pasada++ ) { 
+	} // fin del método init
 
-	         // ciclo para controlar número de comparaciones
-	         for ( int elemento = 0; 
-	               elemento < arreglo2.length - 1; 
-	               elemento++ ) {
+	/**
+	 * Método de Burbuja para ordenas los elementos
+	 *  
+	 * @param arreglo2
+	 */
+	public void ordenamBurbuja(int arreglo2[]) {
+		// ciclo para controlar número de pasadas
+		for (int pasada = 1; pasada < arreglo2.length; pasada++) {
 
-	            // comparar elementos uno a uno e intercambiarlos si 
-	            // el primer elemento es mayor que el segundo
-	            if ( arreglo2[ elemento ] > arreglo2[ elemento + 1 ] )
-	               intercambiar( arreglo2, elemento, elemento + 1 );
+			// ciclo para controlar número de comparaciones
+			for (int elemento = 0; elemento < arreglo2.length - 1; elemento++) {
 
-	         } // fin del ciclo para controlar las comparaciones
+				// comparar elementos uno a uno e intercambiarlos si
+				// el primer elemento es mayor que el segundo
+				if (arreglo2[elemento] > arreglo2[elemento + 1])
+					intercambiar(arreglo2, elemento, elemento + 1);
 
-	      } // fin del ciclo para controlar las pasadas
+			} // fin del ciclo para controlar las comparaciones
 
-	   } // fin del método ordenamBurbuja
+		} // fin del ciclo para controlar las pasadas
 
-	   // intercambiar dos elementos de un arreglo
-	   public void intercambiar( int arreglo3[], int primero, int segundo )
-	   {
-	      int almacen;  // área temporal de almacenamiento para intercambiar
+	} // fin del método ordenamBurbuja
 
-	      almacen = arreglo3[ primero ];         
-	      arreglo3[ primero ] = arreglo3[ segundo ];  
-	      arreglo3[ segundo ] = almacen;
-	   }
+	/**
+	 * Intercambio de 2 elementos de un arreglo
+	 * 
+	 * @param arreglo3
+	 * @param primero
+	 * @param segundo
+	 */
+	public void intercambiar(int arreglo3[], int primero, int segundo) {
+		int almacen; // área temporal de almacenamiento para intercambiar
+
+		almacen = arreglo3[primero];
+		arreglo3[primero] = arreglo3[segundo];
+		arreglo3[segundo] = almacen;
+	}
 }
